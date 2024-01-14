7a4b912294e692003c7dc16c1e18e9d41089cbf1
raihan-rahoof <mcrahoof28@gmail.com>
2024-01-14 15:05:47 +0530

    everything completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import './App.scss';
 import Banner from "./components/banner/Banner";
 import Rowpost from "./components/Rowpost/Rowpost";
-import { originals,Trendings,Action } from "./urls";
+import { originals,Trendings,action,comedy,horror,romance,documentary } from "./urls";
 
 
 function App() {
@@ -13,6 +13,11 @@ function App() {
       <Banner/>
       <Rowpost url={originals} title='Netflix Originals'/>
       <Rowpost url={Trendings} title='Trending' isSmall/>
+      <Rowpost url={action} title='Action' isSmall/>
+      <Rowpost url={comedy} title='Comedy' isSmall/>
+      <Rowpost url={romance} title='Romance' isSmall/>
+      <Rowpost url={horror} title='Horror' isSmall/>
+      <Rowpost url={documentary} title='Documentary' isSmall/>
      </h1>
     </div>
   );
--- src/components/Rowpost/Rowpost.jsx
+++ src/components/Rowpost/Rowpost.jsx
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
 import './Rawpost.scss'
 import axios from '../../axios'
-import { imageUrl } from '../../constants/constants'
+import { imageUrl,API_KEY } from '../../constants/constants'
+import YouTube from 'react-youtube'
 
 function Rowpost(props) {
   const [Trendings,setTrendings] = useState([])
+  const [urlId , setUrlId] = useState('')
   useEffect(() => {
     axios.get(props.url).then((response)=>{
       console.log(response.data)
@@ -13,16 +15,34 @@ function Rowpost(props) {
     
   }, [])
   
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleMovie = (id)=>{
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response =>{
+      if (response.data.results.length !== 0){
+         setUrlId((prevUrlId) => (prevUrlId === response.data.results[0].key ? null : response.data.results[0].key));
+      }else{
+        alert("not available")
+      }
+    }))
+  }
+
   return (
     <div className='Row' >
         <h5>{props.title}</h5>
         <div className="posters">
           {Trendings.map((movie)=>
-          <img className={props.isSmall ? 'small-poster':'poster'} src={`${imageUrl+movie.backdrop_path}`} alt="" />
+          <img onClick={()=> handleMovie(movie.id)} className={props.isSmall ? 'small-poster':'poster'} src={`${imageUrl+movie.backdrop_path}`} alt="" />
           )}
-            
-            
         </div>
+        { urlId && <YouTube opts={opts} videoId={urlId} />}
+
     </div>
   )
 }
